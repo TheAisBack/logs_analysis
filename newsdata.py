2e7cf02d4246f4d89b8a2ac210d95f65c2496146
Theaisback <alanhekle@hotmail.com>
2017-10-01 12:59:57 -0500

    Updating file to fix minor issues with PEP8 style."

--- newsdata.py
+++ newsdata.py
@@ -6,12 +6,15 @@ DBNAME = "news"
 
 # function for getting the most popular articles
 def query_1():
+
     # Connecting to the database
     db = psycopg2.connect(database=DBNAME)
     # running queries to get the results
     c = db.cursor()
-    # execute query_1  
-    c.execute("SELECT title, count(*) AS views FROM articles, log WHERE articles.slug=substring(log.path FROM 10) GROUP BY articles.title ORDER BY views DESC LIMIT 3")
+    # execute query_1
+    c.execute("SELECT title, count(*) AS views FROM articles, log WHERE " +
+              "articles.slug=substring(log.path FROM 10) GROUP BY articles.title " +
+              "ORDER BY views DESC LIMIT 3")
     # Get the results from the cursor
     results = c.fetchall()
     # Prints the 1st Question
@@ -25,12 +28,15 @@ def query_1():
 
 # function for geting the most popular authors
 def query_2():
+
     # Connecting to the database
     db = psycopg2.connect(database=DBNAME)
     # running queries to get the results
     c = db.cursor()
     # execute query_2
-    c.execute("SELECT name, count(*) AS views FROM authors, articles, log WHERE articles.slug = substring(log.path FROM 10) GROUP BY authors.name ORDER BY views DESC LIMIT 4")
+    c.execute("SELECT name, count(*) AS views FROM authors, articles, log WHERE " +
+              "articles.slug = substring(log.path FROM 10) GROUP BY authors.name " +
+              "ORDER BY views DESC LIMIT 4")
     # Get the results from the cursor
     results = c.fetchall()
     # Prints the 2nd Question
@@ -44,12 +50,15 @@ def query_2():
 
 # function for getting the results of 1% or higher in lead errors
 def query_3():
+
     # Connecting to the database
     db = psycopg2.connect(database=DBNAME)
-    # running queries to get the results  
+    # running queries to get the results
     c = db.cursor()
-    # execute query_3  
-    c.execute("SELECT to_char(time, 'FMMonth DD, YYYY'), round(100.0*sum(case log.status when '404 NOT FOUND' then 1 else 0 end)/count(log.status), 2) AS error FROM log GROUP BY to_char(time, 'FMMonth DD, YYYY') ORDER BY error DESC LIMIT 1")
+    # execute query_3
+    c.execute("SELECT to_char(time, 'FMMonth DD, YYYY'), round(100.0*sum(case log.status " +
+              "when '404 NOT FOUND' then 1 else 0 end)/count(log.status), 2) AS error FROM " +
+              "log GROUP BY to_char(time, 'FMMonth DD, YYYY') ORDER BY error DESC LIMIT 1")
     # Get the results from the cursor
     results = c.fetchall()
     # Prints the 3rd Question
@@ -72,5 +81,6 @@ def main():
 
 # Everything correct print results below
 if __name__ == '__main__':
+
     # Calls the function main()
     main()
