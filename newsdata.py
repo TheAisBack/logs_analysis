038cc46618dd44ed29157a3ef5061870b1398fd2
Theaisback <alanhekle@hotmail.com>
2017-10-01 13:49:52 -0500

    PEP8 issues still arise, fixed the file to follow comments to follow protocol.

--- newsdata.py
+++ newsdata.py
@@ -13,8 +13,8 @@ def query_1():
     c = db.cursor()
     # execute query_1
     c.execute("SELECT title, count(*) AS views FROM articles, log WHERE " +
-              "articles.slug=substring(log.path FROM 10) GROUP BY articles.title " +
-              "ORDER BY views DESC LIMIT 3")
+              "articles.slug=substring(log.path FROM 10) GROUP BY " +
+              "articles.title ORDER BY views DESC LIMIT 3")
     # Get the results from the cursor
     results = c.fetchall()
     # Prints the 1st Question
@@ -26,6 +26,7 @@ def query_1():
     db.close()
     # Return results
 
+
 # function for geting the most popular authors
 def query_2():
 
@@ -34,9 +35,9 @@ def query_2():
     # running queries to get the results
     c = db.cursor()
     # execute query_2
-    c.execute("SELECT name, count(*) AS views FROM authors, articles, log WHERE " +
-              "articles.slug = substring(log.path FROM 10) GROUP BY authors.name " +
-              "ORDER BY views DESC LIMIT 4")
+    c.execute("SELECT name, count(*) AS views FROM authors, articles, " +
+              "log WHERE articles.slug = substring(log.path FROM 10) " +
+              "GROUP BY authors.name ORDER BY views DESC LIMIT 4")
     # Get the results from the cursor
     results = c.fetchall()
     # Prints the 2nd Question
@@ -48,6 +49,7 @@ def query_2():
     db.close()
     # Return results
 
+
 # function for getting the results of 1% or higher in lead errors
 def query_3():
 
@@ -56,9 +58,11 @@ def query_3():
     # running queries to get the results
     c = db.cursor()
     # execute query_3
-    c.execute("SELECT to_char(time, 'FMMonth DD, YYYY'), round(100.0*sum(case log.status " +
-              "when '404 NOT FOUND' then 1 else 0 end)/count(log.status), 2) AS error FROM " +
-              "log GROUP BY to_char(time, 'FMMonth DD, YYYY') ORDER BY error DESC LIMIT 1")
+    c.execute("SELECT to_char(time, 'FMMonth DD, YYYY'), round(100.0*" +
+              "sum(case log.status when '404 NOT FOUND' then 1 else 0 " +
+              "end)/count(log.status), 2) AS error FROM log GROUP BY " +
+              "to_char(time, 'FMMonth DD, YYYY') ORDER BY error DESC " +
+              "LIMIT 1")
     # Get the results from the cursor
     results = c.fetchall()
     # Prints the 3rd Question
@@ -70,6 +74,7 @@ def query_3():
     db.close()
     # Return results
 
+
 # main function
 def main():
     # Prints the query_1
@@ -78,6 +83,7 @@ def main():
     query_2()
     # Prints the query_3
     query_3()
+
 
 # Everything correct print results below
 if __name__ == '__main__':
